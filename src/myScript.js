34a0737b84e3a403fb0389235ad40972cdb92ae4
Astha <asthapandey2022jan@cedcoss.com>
2022-02-08 11:49:17 +0530

    check unique

--- src/myScript.js
+++ src/myScript.js
@@ -6,7 +6,7 @@ function addProd() {
     let pprice = document.getElementById("prodPrice").value;
 
     if (checkData(pid, pname, pprice)) {
-        datainput(pid, pname, pprice);
+        checkUnique(pid, pname, pprice);
         display();
     }
 
@@ -16,12 +16,15 @@ function addProd() {
 function checkData(pid, pname, pprice) {
     if (pid == "" || isNaN(pid)) {
         alert("Enter correct ID");
+        document.getElementById("prodId").focus();
     }
     else if (isNaN(pname) === false || pname === "") {
-        alert("Enter valid Name");
+        alert("Field empty or invalid entry!");
+        document.getElementById("prodName").focus();
     }
     else if (isNaN(pprice) || pprice === "") {
-        alert("Enter valid price");
+        alert("Field empty or invalid entry!");
+        document.getElementById("prodPrice").focus();
     }
     else {
         return true;
@@ -30,13 +33,45 @@ function checkData(pid, pname, pprice) {
 }
 
 
-function datainput(pid, pname, pprice) {
-    prodArray.push({
-        "id": pid,
-        "name": pname,
-        "price": pprice
-    });
+function dataInsert(pid, pname, pprice) {  
+            prodArray.push({
+                "id": pid,
+                "name": pname,
+                "price": pprice
+            });
 }
+function checkUnique(pid, pname, pprice){
+    if (prodArray.length == 0){
+        dataInsert(pid, pname, pprice);
+        return;
+    }
+    else{
+    for(let i=0; i<prodArray.length; i++)
+        {
+            if(prodArray[i].id== pid)
+            {
+                alert("ID already exist");
+                return;
+            }            
+        }
+        dataInsert(pid, pname, pprice);
+    }
+}
+
+function dataEdit(){
+    if (prodArray.includes(pid)){
+        var prodArray = getData(pid);
+        document.getElementById("prodId").value = prodId;
+        document.getElementById("addProdBtn").style.display="block"
+    }
+}
+
+function getData(pid){
+    for(let i =0; i= prodArray.length;i++){
+
+    }
+}
+
 
 function display() {
     let result = "";
@@ -45,9 +80,14 @@ function display() {
         document.getElementById("output").innerHTML = "<p>No values</p>";
     }
     else {
-
+        
         for (let i = 0; i < prodArray.length; i++) {
-            result += `<tr><td>${prodArray[i].id}</td><td>${prodArray[i].name}</td><td>${prodArray[i].price}</td></tr>`;
+            result += `<tr>
+            <td>${prodArray[i].id}</td>
+            <td>${prodArray[i].name}</td>
+            <td>${prodArray[i].price}</td>
+            <td><a href ="#" onclick="dataEdit('${prodArray[i].id}')">Edit</a></td>
+            </tr>`;
         }
         document.getElementById("output").innerHTML = `<table>
         <tr>
@@ -57,6 +97,7 @@ function display() {
         </tr>
         ${result}
         </table>`;
+       
 
     }
 }
